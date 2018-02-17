088298547cdc469bd8072f3d34b8047b84f2fb30
Assiljbekov Askhat <enshtein06@gmail.com>
2018-02-17 21:23:56 +0700

    Test the Delete /todos/:id

--- server/server.js
+++ server/server.js
@@ -56,11 +56,11 @@ app.delete('/todos/:id', (req, res) => {
 		return res.status(404).send();
 	}
 
-	Todo.findByIdAndRemove(id).then((docs) => {
-		if(!docs){
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
 			return res.status(404).send();
 		}
-		res.send({docs});
+		res.send({todo});
 	}, (e) => res.status(400).send());
 })
 
