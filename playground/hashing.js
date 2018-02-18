dedc3561eb049e89d27efdac79febf2f951222cc
Assiljbekov Askhat <enshtein06@gmail.com>
2018-02-18 14:45:29 +0700

    Hash user pass before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,58 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+/*
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
+*/
+var hashedPassword = '$2a$10$LIDW0tsIwkBS1zKzp4Xuc.9L5L8/dma8PbyG31wF1/unRaAGexaou';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var data = {
 	id: 10
 };
@@ -9,7 +61,7 @@ var token = jwt.sign(data, '123abc');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+console.log(decoded);*/
 /*
 var message = 'I am user number 3';
 var hash = SHA256(message).toString();
